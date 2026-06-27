fd8c3404ba502a5f48a7255d2780266efbbd4d09
Yulia Soldatkina <yulia.soldatkina22@gmail.com>
2026-06-27 13:17:36 +1000

    Add mask_account_card widget function

---
+++ my-project/src/my_project/widget.py
@@ -0,0 +1,18 @@
+# my-project/src/my_project/widget.py
+
+from .masks import get_mask_card_number, get_mask_account
+
+
+def mask_account_card(raw_data: str) -> str:
+
+    # Разделяем по последнему пробелу: слева имя, справа номер
+    name, number = raw_data.rsplit(" ", 1)
+
+    # Если это счёт
+    if name.startswith("Счет"):
+        masked = get_mask_account(number)
+        return f"{name} {masked}"
+
+    # Иначе считаем, что это карта
+    masked = get_mask_card_number(number)
+    return f"{name} {masked}"
