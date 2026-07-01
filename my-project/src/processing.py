8f5fd38b6941da378d8922d20cb377ed5404084b
Yulia Soldatkina <yulia.soldatkina22@gmail.com>
2026-07-01 13:30:21 +1000

    Добавлены функции filter_by_state и sort_by_date

--- my-project/src/processing.py
+++ my-project/src/processing.py
@@ -1,3 +1,6 @@
+from typing import List, Dict
+
+
 def filter_by_state(items: list[dict], state: str = "EXECUTED") -> list[dict]:
     """
     Возвращает список словарей, у которых ключ 'state' равен указанному значению.
@@ -5,3 +8,12 @@ def filter_by_state(items: list[dict], state: str = "EXECUTED") -> list[dict]:
     :param state: нужное значение ключа 'state' (по умолчанию 'EXECUTED')
     """
     return [item for item in items if item.get("state") == state]
+
+
+def sort_by_date(items: List[Dict], reverse: bool = True) -> List[Dict]:
+    """
+    Возвращает новый список словарей, отсортированный по ключу 'date'.
+    :param items: исходный список словарей
+    :param reverse: порядок сортировки (по умолчанию True — по убыванию)
+    """
+    return sorted(items, key=lambda item: item["date"], reverse=reverse)
