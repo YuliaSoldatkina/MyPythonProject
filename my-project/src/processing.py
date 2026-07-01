ee1b025d8959b4ec461e64b98186563bba25984d
Yulia Soldatkina <yulia.soldatkina22@gmail.com>
2026-07-01 15:25:14 +1000

    Fix flake8 issues

--- my-project/src/processing.py
+++ my-project/src/processing.py
@@ -3,9 +3,8 @@ from typing import List, Dict
 
 def filter_by_state(items: list[dict], state: str = "EXECUTED") -> list[dict]:
     """
-    Возвращает список словарей, у которых ключ 'state' равен указанному значению.
-    :param items: исходный список словарей
-    :param state: нужное значение ключа 'state' (по умолчанию 'EXECUTED')
+     Возвращает список словарей, у которых ключ 'state'
+    равен указанному значению.
     """
     return [item for item in items if item.get("state") == state]
 
